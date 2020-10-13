f38ed66668b46f06681ad861ca794f9905433091
Francisco Ferreira <xikobe@gmail.com>
2020-10-13 18:40:57 +0100

    Updates

---
+++ precache-manifest.312dcfe8f1a69df7d0119a86140eace0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10f2245232197916e731082f030bffcd",
+    "url": "/exchange-app/index.html"
+  },
+  {
+    "revision": "2d1de25fd8d466e1faea",
+    "url": "/exchange-app/static/js/2.9120e932.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/exchange-app/static/js/2.9120e932.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50e3470b855cf844ec61",
+    "url": "/exchange-app/static/js/main.c6fdf6aa.chunk.js"
+  },
+  {
+    "revision": "60fe426c58867b15c388",
+    "url": "/exchange-app/static/js/runtime-main.2db9f9a0.js"
+  }
+]);
